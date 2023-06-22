c35c1eb070e7c92db5630c4517321ebdf352c8d7
Ricardo Mourão <91672048+Ramcmourao@users.noreply.github.com>
2023-06-22 09:42:41 +0100

    Update ProjetoQuiz.cpp

--- ProjetoQuiz.cpp
+++ ProjetoQuiz.cpp
@@ -1,2 +1,22 @@
 #include <stdio.h>
 
+/*
+Função de validar resposta:
+int validaResposta(int a){
+    switch(a){
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+    }
+}
+
+Podem fazer melhor, pesquisem por isto:
+struct Question {
+    string question;
+    string options[4];
+    int correct_option;
+};
+
+STRUCTS
+*/
