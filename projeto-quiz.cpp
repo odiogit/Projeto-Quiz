63105f8355dbf8cbb2bb2333f94bb3646205a620
Diogo Moreira <dsfm97@gmail.com>
2023-06-22 10:26:26 +0100

    Recomeço do Projeto!
    Código com conselhos do Ricardo!

--- projeto-quiz.cpp
+++ projeto-quiz.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale>   
+#include <conio.h>
 
-int main(){
-
-    std::cout << "Olá";
-
-    return 0;
+using namespace std;
+/*
+Função de validar resposta:
+int validaResposta(int a){
+    switch(a){
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+    }
 }
+
+Podem fazer melhor, pesquisem por isto:
+struct Question {
+    string question;
+    string options[4];
+    int correct_option;
+};
+
+STRUCTS
+*/
